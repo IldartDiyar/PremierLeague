1d6faf719447618f8f51e7cfc71ceea698c92efc
Dasd <diyar.ildart@gmail.com>
2022-01-20 00:02:41 +0600

    F

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import './App.css';
-import Footer from './Components/Footer/Footer';
-import NaviBar from './Components/NaviBar/NaviBar';
-import News from './page/Nnews/News';
-import { Route, Routes } from "react-router-dom";
-import Ttable from './page/Ttable/Ttable';
-import Review from './page/Rreview/Review';
-import Main from './page/Main/Main';
-
+import Footer from "./Components/Footer/Footer";
+import NaviBar from "./Components/NaviBar/NaviBar";
+import News from "./page/Nnews/News";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Ttable from "./page/Ttable/Ttable";
+import Review from "./page/Rreview/Review";
+import Main from "./page/Main/Main";
 
 function App() {
   return (
-   <div >
-    <NaviBar/>
-     <Routes>
-        <Route path="/" element={<Main/>}/>
-        <Route path="/news" element={<News/>}/>
-        <Route path="/table" element={<Ttable/>}/>
-        <Route path="/review" element={<Review/>}/>
+    <div>
+      <NaviBar />
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/table" element={<Ttable />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
-    <Footer/>
-   </div>
+      <Footer />
+    </div>
   );
 }
 
